5cd8968a92b6627fd7126fe1555c66a3063125a8
12eyzha <Rayhanbagas27@gmail.com>
2026-01-12 03:49:07 +0700

    add role admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,6 +15,7 @@ class AdminSeeder extends Seeder
             [
                 'name' => 'Admin',
                 'password' => Hash::make('password123'),
+                 'role' => 'admin',
             ]
         );
     }
