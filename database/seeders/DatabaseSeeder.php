d6a87ef83048b3506321e496b7c789159fdaffb3
12eyzha <Rayhanbagas27@gmail.com>
2026-01-12 02:18:04 +0700

    refactor: move admin seeder to AdminSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Seeder user (biarin, ga masalah)
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@senandung.test',
-            'password' => 'password',
-        ]);
-
-        // 🔥 PENTING
         $this->call([
+            AdminSeeder::class,      // ⬅️ ADMIN DIPINDAH KE SINI
             EmployeeSeeder::class,
             CategorySeeder::class,
             MenuSeeder::class,
